135aedac9e86d7b777edf754c03244a63fff98ca
Ntsoccer <2460socceronly0512@gmail.com>
2021-01-22 21:39:39 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const btn = document.getElementById('btn');
+
+
+btn.addEventListener('click', () => AddFizzBuzz());
+
+
+const AddFizzBuzz = function () {
+  const FizzNum = Number(document.getElementById('FizzNum').value);
+  const BuzzNum = Number(document.getElementById('BuzzNum').value);
+  const output = document.getElementById('output');
+
+  if (!Number.isInteger(FizzNum) || !Number.isInteger(BuzzNum) || FizzNum === 0 || BuzzNum === 0) {
+    alert('整数値を入力してください');
+  }
+
+  for (let i = 1; i < 100; i++) {
+    let value = "";
+    if (i % FizzNum === 0 && i % BuzzNum === 0) {
+      value = `FizzBuzz ${i}`;
+    } else if (i % FizzNum === 0) {
+      value = `Fizz ${i}`
+    } else if (i % BuzzNum === 0) {
+      value = `Buzz ${i}`
+    } else {
+      value = ""
+    }
+    const p = document.createElement('p')
+    p.textContent = value;
+    output.appendChild(p);
+  }
+}
